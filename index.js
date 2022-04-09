282e78791022d40281d3ca2a96737db1e883e8d2
BIack1st <steveminecrap69@gmail.com>
2022-04-09 10:19:26 -0500

    made require the cmd list

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 
+const cmdList = require('./CommandsList')
+
 require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
