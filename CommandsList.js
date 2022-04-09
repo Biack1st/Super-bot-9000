09d5ce66538a7d0a5532f5fb2b94b1109a8134a8
BIack1st <steveminecrap69@gmail.com>
2022-04-09 10:41:54 -0500

    readded

---
+++ CommandsList.js
@@ -0,0 +1,31 @@
+let prefix = "!"
+
+const list = [
+    {
+        cmd: `${prefix}bruh`,
+        action: function(msg) {
+            msg.reply("bruh bruh")
+        }
+
+    },
+    {
+        cmd: `${prefix}spamping`,
+        action: function(msg){
+            const repeatTime = 10
+
+            const arg2 = msg.split(" ")[1]
+
+            const replied = arg2
+
+            for (let i = 0; i < repeatTime; i++) {
+                replied += arg2
+            }
+
+            msg.reply(replied)
+        }
+    }
+]
+
+
+
+module.exports = list
