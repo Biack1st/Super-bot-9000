2ba089e4be5ba0b75407548822e993e0d4d3e738
BIack1st <steveminecrap69@gmail.com>
2022-04-09 15:08:15 -0500

    added description

--- CommandsList.js
+++ CommandsList.js
@@ -4,23 +4,35 @@ const Roulettemodule = require("./commands/roulette")
 
 const discord = require("discord.js")
 
+
+
 const list = [
    
     {
         cmd: prefix + `spamping`,
         action: function(msg){
-            const repeatTime = 10
+            const blacklisted = [759136939889655820]
+            const repeatTime = 35
 
             const arg2 = msg.content.split(" ")[1]
 
-            const replied = arg2
+            let replied = arg2
 
             for (let i = 0; i < repeatTime; i++) {
                 replied += arg2
             }
 
-            msg.reply(replied)
-        }
+            if (msg.author.id != 759136939889655820) {
+              
+              msg.reply(replied)
+              
+            }
+            else {
+              msg.reply("you are blacklisted from using this command")
+            }
+            
+        },
+        description: "Spam pings the following user."
     },
     {
       cmd: prefix + "funfact",
@@ -31,14 +43,13 @@ const list = [
 
           data.then(d => d.data)
           .then(d => msg.reply(d))
-      } 
+      },
+      description: "Replies with a random, useless fact." 
     },
     {
         cmd: prefix + `roulette`,
         action: function(msg, client){
           const randomchance = Roulettemodule(1,2)
-
-          console.log(randomchance)
 
           const userMsg = randomchance == 1 && "you win" || "you lose"
             
@@ -70,7 +81,32 @@ const list = [
           msg.reply({ embeds: [embedData] })
 
           
+        },
+        description: "Plays a game of Roulette"
+    },
+    {
+      cmd: prefix + "help",
+      action: function(msg) { 
+        const feilds = []
+        
+        for (let i = 0; i < list.length; i++) {
+            const cmd = list[i]
+            feilds.push({name: cmd.cmd,
+            value: cmd.description || "*no description given*"}) 
         }
+
+
+        const embedData = {
+            color: 0x0099ff,
+
+            author: {
+              name: "Super bot 9000",
+              icon_url: "https://images-ext-1.discordapp.net/external/j0GYtDP6M3F6iKBkAeLUcVPwHcmA5V-u4nmz3MNTg10/https/i.imgur.com/ufhQdix.png"
+            },
+            fields: feilds
+        }
+        msg.reply({embeds: [embedData]})
+      }
     }
 ]
 
