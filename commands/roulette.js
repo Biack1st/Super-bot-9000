701899494fc48facb923b550def2798123a24ad8
BIack1st <steveminecrap69@gmail.com>
2022-04-09 11:21:08 -0500

    fixed error

--- commands/roulette.js
+++ commands/roulette.js
@@ -2,9 +2,9 @@ function ezrandom(min,max){
     return Math.floor(Math.random()*(max,min)+min)
 }
 
-function roulette(min,nax){
- const randomedchance = ezrandom(min,max)
- return randomedchance
+function roulette(min,max){
+    const randomedchance = ezrandom(min,max)
+    return randomedchance
 }
 
 module.exports = roulette
