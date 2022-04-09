e443302d9776b0daafb325c035f488055f8ff669
BIack1st <steveminecrap69@gmail.com>
2022-04-09 10:19:09 -0500

    did something idl

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Client, Intents } = require('discord.js');
 
 require("dotenv").config();
 
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 
