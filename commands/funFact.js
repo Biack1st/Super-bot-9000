96be0d5381e9a05962e5af1c26cf85399ca68173
BIack1st <steveminecrap69@gmail.com>
2022-04-09 11:07:07 -0500

    added fun fact

---
+++ commands/funFact.js
@@ -0,0 +1,11 @@
+const nodeFetch = require("node-fetch")
+
+const url = "https://useless-facts.sameerkumar.website/api"
+
+async function funFact() {
+    const jokePromise = await nodeFetch(url)
+
+    const jokeData = await jokePromise.json()
+
+    return jokeData
+}
