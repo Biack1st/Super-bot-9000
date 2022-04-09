1ec6ddb57b73f9854b789149be1e46e14bfb1302
BIack1st <steveminecrap69@gmail.com>
2022-04-09 10:24:18 -0500

    added base command action code

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+client.on("messageCreate", (msg) => {
+    for (let i = 0; i < cmdList.length; i++) {
+        const cmdData = cmdList[i]
+
+        if (msg.content.match(cmdData.cmd)) {
+            cmdData.action(msg)
+        }
+    }
+})
 
 client.once('ready', () => {
 	console.log('Ready!');
